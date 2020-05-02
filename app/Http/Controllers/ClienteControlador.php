f3c4aebd2747fa529b6d63fcf1af302b216e3dff
Dumon <dumon.dmd@gmail.com>
2020-05-02 12:12:34 -0300

    Armazenando em sessão

--- app/Http/Controllers/ClienteControlador.php
+++ app/Http/Controllers/ClienteControlador.php
@@ -13,6 +13,12 @@ class ClienteControlador extends Controller
         ['id'=>3, 'nome'=>'Nome_3'],
         ['id'=>4, 'nome'=>'Nome_4']
     ];
+
+    public function __construct(){
+        $clientes = session('clientes');
+        if(!isset($clientes))
+            session(['clientes' => $this->clientes]);
+    }
     /**
      * Display a listing of the resource.
      *
@@ -20,7 +26,7 @@ class ClienteControlador extends Controller
      */
     public function index()
     {
-        $clientes = $this->clientes;
+        $clientes = session('clientes');
         return view('clientes.index', compact(['clientes']));
     }
 
@@ -42,15 +48,13 @@ class ClienteControlador extends Controller
      */
     public function store(Request $request)
     {
-        $id = count($this->clientes)+1;
+        $clientes = session('clientes');
+        $id = count($clientes)+1;
         $nome = $request->nome;
         $dados = ["id"=>$id, "nome"=>$nome];
-        $this->clientes[] = $dados;
-
-        //return redirect()->route('clientes.index');
-        $clientes = $this->clientes;
-        return view('clientes.index', compact(['clientes']));
-        
+        $clientes[] = $dados;
+        session(['clientes' => $clientes]);
+        return redirect()->route('clientes.index');               
     }
 
     /**
