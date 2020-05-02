8fc76ec41ec7bcd2e58265ad97dd36d37dac7f4b
Dumon <dumon.dmd@gmail.com>
2020-05-02 15:03:05 -0300

    Correção do index

--- app/Http/Controllers/ClienteControlador.php
+++ app/Http/Controllers/ClienteControlador.php
@@ -49,7 +49,7 @@ class ClienteControlador extends Controller
     public function store(Request $request)
     {
         $clientes = session('clientes');
-        $id = count($clientes)+1;
+        $id = end($clientes)['id']+1;
         $nome = $request->nome;
         $dados = ["id"=>$id, "nome"=>$nome];
         $clientes[] = $dados;
@@ -66,7 +66,8 @@ class ClienteControlador extends Controller
     public function show($id)
     {
         $clientes =  session('clientes');
-        $cliente = $clientes[ $id - 1];
+        $index = $this->getIndex($id, $clientes); 
+        $cliente = $clientes[ $index];
         return view('clientes.info', compact(['cliente']));
     }
 
@@ -79,7 +80,8 @@ class ClienteControlador extends Controller
     public function edit($id)
     {
         $clientes = session('clientes');
-        $cliente = $clientes[ $id - 1];
+        $index = $this->getIndex($id, $clientes); 
+        $cliente = $clientes[ $index];
         return view('clientes.edit', compact(['cliente']));
     }
 
@@ -93,7 +95,8 @@ class ClienteControlador extends Controller
     public function update(Request $request, $id)
     {
         $clientes = session('clientes');
-        $clientes[$id - 1]['nome'] = $request->nome;
+        $index = $this->getIndex($id, $clientes); 
+        $clientes[$index]['nome'] = $request->nome;
         session(['clientes' => $clientes]);
         return redirect()->route('clientes.index');
     }
@@ -107,10 +110,15 @@ class ClienteControlador extends Controller
     public function destroy($id)
     {
         $clientes = session('clientes');
-        $ids = array_column($clientes, 'id');
-        $index = array_search($id, $ids);
+        $index = $this->getIndex($id, $clientes);        
         array_splice($clientes, $index, 1);
         session(['clientes' => $clientes]);
         return redirect()->route('clientes.index');
+    }
+
+    private function getIndex($id, $clientes) {
+        $ids = array_column($clientes, 'id');
+        $index = array_search($id, $ids);
+        return $index;
     }
 }
--- resources/views/clientes/index.blade.php
+++ resources/views/clientes/index.blade.php
@@ -2,7 +2,7 @@
 <h3><a href="{{route('clientes.create')}}">Novo cliente</a></h3>
 <ul>
 	@foreach($clientes as $c)
-		<li>{{$c['nome']}} |
+		<li>{{$c['id']}} | {{$c['nome']}} |
 			<a href="{{route('clientes.edit', $c['id'])}}">Editar</a>|
 			<a href="{{route('clientes.show', $c['id'])}}">Info</a>
 			<form action="{{route('clientes.destroy', $c['id'])}}" method="POST">
