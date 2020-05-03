dc84acb814d89d60dfa7ea1a0b480f1b3a14bb7d
Dumon <dumon.dmd@gmail.com>
2020-05-03 12:03:50 -0300

    Passagem de parâmetros para componentes

--- resources/views/outras/departamentos.blade.php
+++ resources/views/outras/departamentos.blade.php
@@ -9,8 +9,24 @@
 	<li>Roupas</li>
 </ul>
 
-@component('componentes.alerta')
+@component('componentes.alerta', ['titulo'=>'Erro Fatal', 'tipo'=>'info'])
+<p><strong>Erro inesperado</strong></p>
+<p>Ocorreu um erro inesperado</p>
+@endcomponent
 
+@component('componentes.alerta', ['titulo'=>'Erro Fatal', 'tipo'=>'error'])
+<p><strong>Erro inesperado</strong></p>
+<p>Ocorreu um erro inesperado</p>
+@endcomponent
+
+@component('componentes.alerta', ['titulo'=>'Erro Fatal', 'tipo'=>'success'])
+<p><strong>Erro inesperado</strong></p>
+<p>Ocorreu um erro inesperado</p>
+@endcomponent
+
+@component('componentes.alerta', ['titulo'=>'Erro Fatal', 'tipo'=>'warning'])
+<p><strong>Erro inesperado</strong></p>
+<p>Ocorreu um erro inesperado</p>
 @endcomponent
 
 @endsection
